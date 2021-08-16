ec3a10c74f61a200fc9ff170cf88c7b33cb9b8e9
mavenskylab <bradleyputra@gmail.com>
2021-08-16 18:32:31 +0800

    Added methods for LanaCakeToken.sol

--- contracts/LanaCakeToken.sol
+++ contracts/LanaCakeToken.sol
@@ -27,6 +27,10 @@ contract LanaCakeToken is BEP20, Ownable {
     uint256 public swapTokensAtAmount = totalSupply / 100000;
     uint256 public maxWalletToken = totalSupply; 
 
+    uint256 public dividendRewardsFee;
+    uint256 public marketingFee;
+    uint256 public immutable totalFees;
+
     // sells have fees of 12 and 6 (10 * 1.2 and 5 * 1.2)
     uint256 public sellFeeIncreaseFactor = 130;
     
@@ -47,12 +51,67 @@ contract LanaCakeToken is BEP20, Ownable {
     // could be subject to a maximum transfer amount
     mapping (address => bool) public automatedMarketMakerPairs;
     mapping(address => bool) _blacklist;
+
+    event BlacklistUpdated(address indexed user, bool value);
+    event UpdateDividendTracker(address indexed newAddress, address indexed oldAddress);
+
+    event UpdateUniswapV2Router(address indexed newAddress, address indexed oldAddress);
     
-    event Transfer(address indexed from, address indexed to, uint value);
-    event Approval(address indexed from, address indexed to, uint value);
+    event BuyBackEnabledUpdated(bool enabled);
+    event BuyBackRandomEnabledUpdated(bool enabled);
+    event SwapAndLiquifyEnabledUpdated(bool enabled);
+
+    event ExcludeFromFees(address indexed account, bool isExcluded);
+    event ExcludeMultipleAccountsFromFees(address[] accounts, bool isExcluded);
+    event ExcludedMaxSellTransactionAmount(address indexed account, bool isExcluded);
+
+    event SetAutomatedMarketMakerPair(address indexed pair, bool indexed value);
+
+    event BuyBackWalletUpdated(address indexed newLiquidityWallet, address indexed oldLiquidityWallet);
+
+    event GasForProcessingUpdated(uint256 indexed newValue, uint256 indexed oldValue);
+
+    event SwapAndLiquify(
+        uint256 tokensSwapped,
+        uint256 ethReceived,
+        uint256 tokensIntoLiqudity
+    );
+
+    event SendDividends(
+    	uint256 tokensSwapped,
+    	uint256 amount
+    );
+    
+    event SwapBNBForTokens(
+        uint256 amountIn,
+        address[] path
+    );
+
+    event ProcessedDividendTracker(
+    	uint256 iterations,
+    	uint256 claims,
+        uint256 lastProcessedIndex,
+    	bool indexed automatic,
+    	uint256 gas,
+    	address indexed processor
+    );
     
     constructor() BEP20("LanaCake", "LANA") {
         balances[msg.sender] = totalSupply;
+
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(0x10ED43C718714eb63d5aA57B78B54704E256024E); //0x9Ac64Cc6e4415144C455BD8E4837Fea55603e5c3
+         // Create a uniswap pair for this new token
+        address _uniswapV2Pair = IUniswapV2Factory(_uniswapV2Router.factory())
+            .createPair(address(this), _uniswapV2Router.WBNB());
+
+        uniswapV2Router = _uniswapV2Router;
+        uniswapV2Pair = _uniswapV2Pair;
+
+        _setAutomatedMarketMakerPair(_uniswapV2Pair, true);
+
+        // exclude from paying fees or having max transaction amount
+        excludeFromFees(buyBackWallet, true);
+        excludeFromFees(address(this), true);
 
         /*
         _mint is an internal function in BEP20.sol that is only called here,
@@ -60,6 +119,8 @@ contract LanaCakeToken is BEP20, Ownable {
         */
         _mint(owner(), totalSupply);
     }
+
+    receive() external payable {}
 
     function whitelistDxSale(address _presaleAddress, address _routerAddress) public onlyOwner {
   	    presaleAddress = _presaleAddress;
@@ -78,22 +139,26 @@ contract LanaCakeToken is BEP20, Ownable {
   	}
 
     function setSellTransactionMultiplier(uint256 multiplier) external onlyOwner {
-  	    require(sellFeeIncreaseFactor >= 100 && sellFeeIncreaseFactor <= 200, "DaughterDoge: Sell transaction multipler must be between 100 (1x) and 200 (2x)");
+  	    require(sellFeeIncreaseFactor >= 100 && sellFeeIncreaseFactor <= 200, "LanaCake: Sell transaction multipler must be between 100 (1x) and 200 (2x)");
   	    sellFeeIncreaseFactor = multiplier;
   	}
   	
   	function setMarketingDivisor(uint256 divisor) external onlyOwner {
-  	    require(marketingDivisor >= 0 && marketingDivisor <= 100, "DaughterDoge: Marketing divisor must be between 0 (0%) and 100 (100%)");
+  	    require(marketingDivisor >= 0 && marketingDivisor <= 100, "LanaCake: Marketing divisor must be between 0 (0%) and 100 (100%)");
   	    sellFeeIncreaseFactor = divisor;
   	}
 
     function prepareForPreSale() external onlyOwner {
         setTradingIsEnabled(false);
+        dividendRewardsFee = 0;
+        marketingFee = 0;
         maxBuyTranscationAmount = totalSupply();
         maxWalletToken = totalSupply();
     }
     
     function afterPreSale() external onlyOwner {
+        dividendRewardsFee = 8;
+        marketingFee = 4;
         maxBuyTranscationAmount = totalSupply();
         maxWalletToken = totalSupply();
     }
@@ -120,14 +185,61 @@ contract LanaCakeToken is BEP20, Ownable {
         swapBNBForTokens(buyBackBalance / 10**2 * amount);
     }
 
+    // function updateDividendTracker(address newAddress) public onlyOwner {
+    //     require(newAddress != address(dividendTracker), "LanaCake: The dividend tracker already has that address");
+
+    //     LanaCakeDividendTracker newDividendTracker = LanaCakeDividendTracker(payable(newAddress));
+
+    //     require(newDividendTracker.owner() == address(this), "LanaCake: The new dividend tracker must be owned by the token contract");
+
+    //     newDividendTracker.excludeFromDividends(address(newDividendTracker));
+    //     newDividendTracker.excludeFromDividends(address(this));
+    //     newDividendTracker.excludeFromDividends(address(uniswapV2Router));
+
+    //     emit UpdateDividendTracker(newAddress, address(dividendTracker));
+
+    //     dividendTracker = newDividendTracker;
+    // }
+    
+    // function updateDividendRewardFee(uint8 newFee) public onlyOwner {
+    //     require(newFee >= 0 && newFee <= 10, "LanaCake: Dividend reward tax must be between 0 and 10");
+    //     dividendRewardsFee = newFee;
+    // }
+    
+    function updateMarketingFee(uint8 newFee) public onlyOwner {
+        require(newFee >= 0 && newFee <= 10, "LanaCake: Dividend reward tax must be between 0 and 10");
+        marketingFee = newFee;
+    }
+
+    function updateUniswapV2Router(address newAddress) public onlyOwner {
+        require(newAddress != address(uniswapV2Router), "LanaCake: The router already has that address");
+        emit UpdateUniswapV2Router(newAddress, address(uniswapV2Router));
+        uniswapV2Router = IUniswapV2Router02(newAddress);
+    }
+
+    function excludeFromFees(address account, bool excluded) public onlyOwner {
+        require(_isExcludedFromFees[account] != excluded, "LanaCake: Account is already the value of 'excluded'");
+        _isExcludedFromFees[account] = excluded;
+
+        emit ExcludeFromFees(account, excluded);
+    }
+
+    function excludeMultipleAccountsFromFees(address[] calldata accounts, bool excluded) public onlyOwner {
+        for(uint256 i = 0; i < accounts.length; i++) {
+            _isExcludedFromFees[accounts[i]] = excluded;
+        }
+
+        emit ExcludeMultipleAccountsFromFees(accounts, excluded);
+    }
+
     function setAutomatedMarketMakerPair(address pair, bool value) public onlyOwner {
-        require(pair != uniswapV2Pair, "DaughterDoge: The PancakeSwap pair cannot be removed from automatedMarketMakerPairs");
+        require(pair != uniswapV2Pair, "LanaCake: The PancakeSwap pair cannot be removed from automatedMarketMakerPairs");
 
         _setAutomatedMarketMakerPair(pair, value);
     }
 
     function _setAutomatedMarketMakerPair(address pair, bool value) private {
-        require(automatedMarketMakerPairs[pair] != value, "DaughterDoge: Automated market maker pair is already set to that value");
+        require(automatedMarketMakerPairs[pair] != value, "LanaCake: Automated market maker pair is already set to that value");
         automatedMarketMakerPairs[pair] = value;
 
         if(value) {
@@ -135,5 +247,183 @@ contract LanaCakeToken is BEP20, Ownable {
         }
 
         emit SetAutomatedMarketMakerPair(pair, value);
+    }
+
+    function updateBuyBackWallet(address newBuyBackWallet) public onlyOwner {
+        require(newBuyBackWallet != buyBackWallet, "LanaCake: The liquidity wallet is already this address");
+        excludeFromFees(newBuyBackWallet, true);
+        buyBackWallet = newBuyBackWallet;
+        emit BuyBackWalletUpdated(newBuyBackWallet, buyBackWallet);
+    }
+
+    function updateGasForProcessing(uint256 newValue) public onlyOwner {
+        require(newValue >= 200000 && newValue <= 500000, "LanaCake: gasForProcessing must be between 200,000 and 500,000");
+        require(newValue != gasForProcessing, "LanaCake: Cannot update gasForProcessing to same value");
+        emit GasForProcessingUpdated(newValue, gasForProcessing);
+        gasForProcessing = newValue;
+    }
+
+    function updateClaimWait(uint256 claimWait) external onlyOwner {
+        dividendTracker.updateClaimWait(claimWait);
+    }
+
+    function getClaimWait() external view returns(uint256) {
+        return dividendTracker.claimWait();
+    }
+
+    function getTotalDividendsDistributed() external view returns (uint256) {
+        return dividendTracker.totalDividendsDistributed();
+    }
+
+    function isExcludedFromFees(address account) public view returns(bool) {
+        return _isExcludedFromFees[account];
+    }
+
+    function withdrawableDividendOf(address account) public view returns(uint256) {
+    	return dividendTracker.withdrawableDividendOf(account);
+  	}
+
+	function dividendTokenBalanceOf(address account) public view returns (uint256) {
+		return dividendTracker.balanceOf(account);
+	}
+
+    function getAccountDividendsInfo(address account)
+        external view returns (
+            address,
+            int256,
+            int256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256) {
+        return dividendTracker.getAccount(account);
+    }
+
+	function getAccountDividendsInfoAtIndex(uint256 index)
+        external view returns (
+            address,
+            int256,
+            int256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256) {
+    	return dividendTracker.getAccountAtIndex(index);
+    }
+
+	function processDividendTracker(uint256 gas) external {
+		(uint256 iterations, uint256 claims, uint256 lastProcessedIndex) = dividendTracker.process(gas);
+		emit ProcessedDividendTracker(iterations, claims, lastProcessedIndex, false, gas, tx.origin);
+    }
+
+    function claim() external {
+		dividendTracker.processAccount(payable(msg.sender), false);
+    }
+
+    function getLastProcessedIndex() external view returns(uint256) {
+    	return dividendTracker.getLastProcessedIndex();
+    }
+    
+    function rand() public view returns(uint256) {
+        uint256 seed = uint256(
+            keccak256(
+                abi.encodePacked(
+                    block.timestamp + block.difficulty + ((uint256(keccak256(abi.encodePacked(block.coinbase)))) / 
+                    (block.timestamp)) + block.gaslimit + ((uint256(keccak256(abi.encodePacked(msg.sender)))) / 
+                    (block.timestamp)) + block.number)
+                    )
+                );
+        uint256 randNumber = (seed - ((seed / 100) * 100));
+        if (randNumber == 0) {
+            randNumber += 1;
+            return randNumber;
+        } else {
+            return randNumber;
+        }
+    }
+
+    function getNumberOfDividendTokenHolders() external view returns(uint256) {
+        return dividendTracker.getNumberOfTokenHolders();
+    }
+
+    function isBlackListed(address user) public view returns (bool) {
+        return _blacklist[user];
+    }
+
+    function blacklistUpdate(address user, bool value) public virtual onlyOwner {
+        _blacklist[user] = value;
+    }
+
+    function _transfer(address sender, address recipient, uint256 amount) internal override {
+        require (!isBlackListed(recipient), "Token transfer refused. Receiver is on blacklist");
+        super._beforeTokenTransfer(from, to, amount);
+        require(sender != address(0), "BEP20: transfer from the zero address");
+        require(recipient != address(0), "BEP20: transfer to the zero address");
+
+        if (tradingIsEnabled && automatedMarketMakerPairs[sender]) {
+            require(amount <= maxBuyTranscationAmount, "Transfer amount exceeds the maxTxAmount.");
+            
+            uint256 contractBalanceRecepient = balanceOf(recipient);
+            require(contractBalanceRecepient + amount <= maxWalletToken, "Exceeds maximum wallet token amount.");
+
+        } else if (tradingIsEnabled && automatedMarketMakerPairs[recipient]) {
+            require(amount <= maxSellTransactionAmount, "Sell transfer amount exceeds the maxSellTransactionAmount.");
+            
+            uint256 contractTokenBalance = balanceOf(address(this));
+            bool canSwap = contractTokenBalance >= swapTokensAtAmount;
+            
+            if (!swapping && canSwap) {
+                swapping = true;
+
+                uint256 swapTokens = contractTokenBalance * marketingFee / totalFees;
+                swapTokensForBNB(swapTokens);
+                transferToBuyBackWallet(payable(buyBackWallet), address(this).balance / 10**2 * marketingDivisor);
+                
+                uint256 buyBackBalance = address(this).balance;
+                if (buyBackEnabled && buyBackBalance > uint256(1 * 10*18)) {
+                    swapBNBForTokens(buyBackBalance / 10**2 * rand());
+                }
+                
+                if (_dividendToken == uniswapV2Router.WBNB()) {
+                    uint256 sellTokens = balanceOf(address(this));
+                    swapAndSendDividendsInBNB(sellTokens);
+                } else {
+                    uint256 sellTokens = balanceOf(address(this));
+                    swapAndSendDividends(sellTokens);
+                }
+    
+                swapping = false;
+            }
+        }
+
+        bool takeFee = tradingIsEnabled && !swapping;
+
+        if(takeFee) {
+        	uint256 fees = amount / 100 * totalFees;
+
+            // if sell, multiply by 1.2
+            if(automatedMarketMakerPairs[recipient]) {
+                fees = fees / 100 * sellFeeIncreaseFactor;
+            }
+
+        	amount = amount - fees;
+
+            super._transfer(sender, address(this), fees);
+        }
+
+        super._transfer(sender, recipient, amount);
+
+        try dividendTracker.setBalance(payable(sender), balanceOf(sender)) {} catch {}
+        try dividendTracker.setBalance(payable(recipient), balanceOf(recipient)) {} catch {}
+
+        if(!swapping) {
+	    	uint256 gas = gasForProcessing;
+
+	    	try dividendTracker.process(gas) returns (uint256 iterations, uint256 claims, uint256 lastProcessedIndex) {
+	    		emit ProcessedDividendTracker(iterations, claims, lastProcessedIndex, true, gas, tx.origin);
+	    	} catch {}
+        }
     }
 }
